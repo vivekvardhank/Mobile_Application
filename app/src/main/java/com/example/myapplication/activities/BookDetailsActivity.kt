b5e5d56c12768aa650885cf72da83c609dbbe29f
vivek vardhan kalyanapu <kvivek3333@gmail.com>
2023-04-14 00:40:58 -0500

    ui changes

--- app/src/main/java/com/example/myapplication/activities/BookDetailsActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/BookDetailsActivity.kt
@@ -1,12 +1,25 @@
 package com.example.myapplication.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.myapplication.R
 
 class BookDetailsActivity : AppCompatActivity() {
+    private var textView: TextView? = null
+    private var imageView: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_details)
+
+        textView = findViewById(R.id.bookDetailsName)
+        imageView = findViewById(R.id.bookDetailsImage)
+
+        val intent: Intent? = getIntent()
+        textView!!.setText(intent?.getStringExtra("name"))
+        intent?.getIntExtra("Image",0)?.let { imageView!!.setImageResource(it) }
+
     }
 }
